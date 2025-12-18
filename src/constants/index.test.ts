cc326865f17892e643d715170d53d31016dc4013
jusmccar <justinmccartney1@yahoo.com>
2025-12-18 11:18:12 -0800

    Add divide function and tests

--- src/constants/index.test.ts
+++ src/constants/index.test.ts
@@ -1,7 +1,17 @@
-import { COURSE_NAME } from '@app/constants/index';
+import { COURSE_NAME, divide } from '@app/constants/index';
 
 describe('Constants', () => {
   it('Should return the correct value for COURSE_NAME', () => {
     expect(COURSE_NAME).toBe('Advanced Testing with React, Jest, Vitest, and Playwright');
+  });
+});
+
+describe('Divide', () => {
+  it('Should return the value of a divided by b', () => {
+    expect(divide(4, 2)).toBe(2);
+  });
+
+  it('Should throw when attempting to divide by zero', () => {
+    expect(() => divide(4, 0)).toThrow('Division by zero is not possible');
   });
 });
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,3 +3,11 @@ export const API_URL = 'http://localhost:3001';
 export const API_URL_POSTS = 'http://localhost:3001/posts/';
 
 export const COURSE_NAME = 'Advanced Testing with React, Jest, Vitest, and Playwright';
+
+export const divide = (a: number, b: number): number => {
+  if (b === 0) {
+    throw new Error('Division by zero is not possible');
+  }
+
+  return a / b;
+};
