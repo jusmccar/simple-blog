50cfee13ade5b5e5dcd00ac279bf72894f680a5e
jusmccar <justinmccartney1@yahoo.com>
2025-12-22 18:07:43 -0800

    Add mock for window.matchMedia

--- src/setup.ts
+++ src/setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+if (typeof window.matchMedia !== 'function') {
+  window.matchMedia = (query): MediaQueryList => {
+    return {
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: (): void => {},
+      removeListener: (): void => {},
+      addEventListener: (): void => {},
+      removeEventListener: (): void => {},
+      dispatchEvent: (): boolean => false,
+    };
+  };
+}
