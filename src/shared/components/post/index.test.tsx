3e1f22811cd15e31872b34eced8e76efc6154d48
jusmccar <justinmccartney1@yahoo.com>
2025-12-22 18:23:31 -0800

    Add Post image and avatar tests

--- src/shared/components/post/index.test.tsx
+++ src/shared/components/post/index.test.tsx
@@ -10,4 +10,24 @@ describe('Post', () => {
 
     expect(screen.getByText(post.title)).toBeInTheDocument();
   });
+
+  it('Renders the featured image of the post', () => {
+    const post = getTestPost();
+    render(<Post post={post} />);
+
+    const image = screen.getByAltText(post.title);
+
+    expect(image).toBeInTheDocument();
+    expect(image).toHaveAttribute('src', post.image?.url);
+  });
+
+  it("Renders the author's avatar of the post", () => {
+    const post = getTestPost();
+    render(<Post post={post} />);
+
+    const images = screen.getAllByRole('img');
+
+    expect(images).toHaveLength(2);
+    expect(images[1]).toHaveAttribute('src', post.author.avatarUrl);
+  });
 });
