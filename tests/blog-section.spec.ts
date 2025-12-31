64e8fd86013aa7eb764f9f009b657d4c2787be30
jusmccar <justinmccartney1@yahoo.com>
2025-12-31 09:41:06 -0800

    Add 'users can see a post by clicking on it' playwright test

--- tests/blog-section.spec.ts
+++ tests/blog-section.spec.ts
@@ -17,3 +17,23 @@ test('has correct heading', async ({ page }) => {
   // Expect h1 to be the correct string.
   await expect(h1).toHaveText('Simple Blog Created by React');
 });
+
+test('users can see a post by clicking on it', async ({ page }) => {
+  await page.goto(URL);
+
+  // Get main section.
+  const main = page.locator('main');
+
+  // Get all links in main section.
+  const links = main.getByRole('link');
+
+  await expect(links).toHaveCount(3);
+
+  await expect(links.nth(0)).toHaveText('5 Mouth-Watering Recipes You Need to Try This Week');
+
+  await links.nth(0).click();
+
+  const h2 = page.locator('h2');
+
+  await expect(h2).toHaveText('5 Mouth-Watering Recipes You Need to Try This Week');
+});
