ac47d933750638be937cc9b49c1c6e5d2e0b9331
jusmccar <justinmccartney1@yahoo.com>
2025-12-30 17:17:30 -0800

    Refactor ListPosts tests

--- src/admin/components/posts/listPosts.test.tsx
+++ src/admin/components/posts/listPosts.test.tsx
@@ -14,21 +14,9 @@ vi.mock('@app/api/posts/usePosts');
 describe('<ListPosts />', () => {
   const queryClient = new QueryClient();
 
-  it('Renders loading if the posts have not been loaded yet', () => {
-    (usePosts as Mock).mockReturnValue({
-      data: [],
-      isLoading: true,
-      error: null,
-    });
-
-    render(<ListPosts />);
-
-    expect(screen.getByText('Loading...')).toBeInTheDocument();
-  });
-
-  it('Renders a table with a list of posts', () => {
+  const setupMockedPosts = (minNumberOfPosts: number = 1, maxNumberOfPosts: number = 20) => {
     const mockedPosts: PostType[] = Array.from(
-      { length: faker.number.int({ min: 1, max: 20 }) },
+      { length: faker.number.int({ min: minNumberOfPosts, max: maxNumberOfPosts }) },
       () => getTestPost(),
     );
 
@@ -46,6 +34,50 @@ describe('<ListPosts />', () => {
       </QueryClientProvider>,
     );
 
+    return mockedPosts;
+  };
+
+  it('Renders loading if the posts have not been loaded yet', () => {
+    (usePosts as Mock).mockReturnValue({
+      data: [],
+      isLoading: true,
+      error: null,
+    });
+
+    render(<ListPosts />);
+
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
+  });
+
+  it('Renders a table with a list of posts', () => {
+    const mockedPosts = setupMockedPosts(1, 10);
+
     expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+
+    const rows = screen.getAllByRole('row');
+
+    expect(rows.length).toBe(mockedPosts.length + 1);
+
+    expect(screen.getByText('#')).toBeInTheDocument();
+    expect(screen.getByText('Featured Image')).toBeInTheDocument();
+    expect(screen.getByText('Title')).toBeInTheDocument();
+    expect(screen.getByText('Action')).toBeInTheDocument();
+
+    const links = screen.getAllByRole('link');
+    const deleteButtons = screen.getAllByRole('button', { name: 'Delete' });
+
+    expect(screen.getByText(mockedPosts[0].title)).toBeInTheDocument();
+    expect(links[0]).toHaveAttribute('href', `/admin/post/edit/${mockedPosts[0].id}`);
+    expect(deleteButtons).toHaveLength(mockedPosts.length);
+  });
+
+  it('Renders a table with pagination if the number of posts is more than 10', () => {
+    const mockedPosts = setupMockedPosts(11, 20);
+
+    expect(screen.queryByText('Loading...')).not.toBeInTheDocument();
+
+    const rows = screen.getAllByRole('row');
+
+    expect(rows.length).toBe(11);
   });
 });
