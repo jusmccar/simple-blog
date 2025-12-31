49a02d9bd478fb98d8928a9d3a59e74c19c3477e
jusmccar <justinmccartney1@yahoo.com>
2025-12-31 09:21:45 -0800

    Add correct heading playwright test

--- tests/blog-section.spec.ts
+++ tests/blog-section.spec.ts
@@ -8,3 +8,12 @@ test('has title', async ({ page }) => {
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Simple BlogPage/);
 });
+
+test('has correct heading', async ({ page }) => {
+  await page.goto(URL);
+
+  const h1 = page.locator('h1');
+
+  // Expect h1 to be the correct string.
+  await expect(h1).toHaveText('Simple Blog Created by React');
+});
